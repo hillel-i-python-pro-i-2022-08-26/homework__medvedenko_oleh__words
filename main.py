f9fb61bfe83090af1fc114941c95d163c668a067
chtholine <oleg123qwe@gmail.com>
2023-02-20 01:01:54 +0200

    Major generation changes async

--- main.py
+++ main.py
@@ -1,50 +1,33 @@
-import logging
-import pathlib
-from string import ascii_lowercase, digits
-from typing import Generator, Iterable
-
-STORAGE = pathlib.Path(__file__).parent.joinpath("words_storage")
+import asyncio
+import string
 
 
-def init_logger() -> logging.Logger:
-    logger = logging.getLogger()
-    logging.basicConfig(level=logging.INFO)
-    return logger
-
-
-class Combinator:
-    def __init__(
-        self,
-        alphabet: Iterable,
-        str_len: int,
-        logger: logging.Logger = init_logger(),
-    ):
+class ProductGenerator:
+    def __init__(self, alphabet, repeats, filename):
         self.alphabet = alphabet
-        self.str_len = abs(str_len)
-        self.logger = logger
+        self.repeats = repeats
+        self.filename = filename
 
-    def generate_combinations(self) -> Generator[str, None, None]:
-        self.logger.info("[START] Combining started [START]")
-        yield from self._generate_combinations_recursive("")
-        self.logger.info(
-            "[END] All combinations created successfully. See result.txt file [END]"
-        )
-
-    def _generate_combinations_recursive(
-        self, prefix: str
-    ) -> Generator[str, None, None]:
-        if len(prefix) == self.str_len:
-            yield prefix
+    async def write_combinations(self, prefix=""):
+        if len(prefix) == self.repeats:
+            with open(self.filename, "a") as f:
+                f.write(prefix + "\n")
             return
 
         for char in self.alphabet:
-            yield from self._generate_combinations_recursive(prefix + char)
+            await self.write_combinations(prefix + char)
+
+    async def generate_products(self):
+        with open(self.filename, "w"):
+            pass  # clear the file if it already exists
+
+        tasks = [self.write_combinations(char) for char in self.alphabet]
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    repeats = 5
-    alphabet_for_combinations = ascii_lowercase + digits
-    combinator = Combinator(alphabet_for_combinations, repeats)
-    with open(STORAGE.joinpath("result/result.txt"), mode="w") as file_result:
-        for combination in combinator.generate_combinations():
-            file_result.write(combination + "\n")
+    alphabet = string.ascii_lowercase + string.digits
+    repeats = 6
+    filename = "products.txt"
+    pg = ProductGenerator(alphabet, repeats, filename)
+    asyncio.run(pg.generate_products())
