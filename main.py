6c7ab1ca01a9a8c2141b99f857c4cede282e46b2
chtholine <oleg123qwe@gmail.com>
2023-02-20 08:13:53 +0200

    Major generation changes async

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
+import logging
+import pathlib
 import asyncio
-import string
+from string import ascii_lowercase, digits
+from typing import AsyncGenerator, Iterable
+import aiofiles
+
+STORAGE = pathlib.Path(__file__).parent.joinpath("words_storage")
 
 
-class ProductGenerator:
-    def __init__(self, alphabet, repeats, filename):
+def init_logger() -> logging.Logger:
+    logger = logging.getLogger()
+    logging.basicConfig(level=logging.INFO)
+    return logger
+
+
+class Combinator:
+    def __init__(
+        self,
+        alphabet: Iterable,
+        str_len: int,
+        logger: logging.Logger = init_logger(),
+    ):
         self.alphabet = alphabet
-        self.repeats = repeats
-        self.filename = filename
+        self.str_len = abs(str_len)
+        self.logger = logger
 
-    async def write_combinations(self, prefix=""):
-        if len(prefix) == self.repeats:
-            with open(self.filename, "a") as f:
-                f.write(prefix + "\n")
+    async def generate_combinations(self) -> AsyncGenerator[str, None]:
+        self.logger.info("[START] Combining started [START]")
+        async for combination in self._generate_combinations_recursive(""):
+            yield combination
+        self.logger.info(
+            "[END] All combinations created successfully. See result.txt file [END]"
+        )
+
+    async def _generate_combinations_recursive(
+        self, prefix: str
+    ) -> AsyncGenerator[str, None]:
+        if len(prefix) == self.str_len:
+            yield prefix
             return
 
         for char in self.alphabet:
-            await self.write_combinations(prefix + char)
+            async for combination in self._generate_combinations_recursive(prefix + char):
+                yield combination
 
-    async def generate_products(self):
-        with open(self.filename, "w"):
-            pass  # clear the file if it already exists
 
-        tasks = [self.write_combinations(char) for char in self.alphabet]
-        await asyncio.gather(*tasks)
+async def main():
+    repeats = 3
+    alphabet_for_combinations = ascii_lowercase + digits
+    combinator = Combinator(alphabet_for_combinations, repeats)
+    async with aiofiles.open(STORAGE.joinpath("result/result.txt"), mode="w") as file_result:
+        async for combination in combinator.generate_combinations():
+            await file_result.write(combination + "\n")
 
 
 if __name__ == "__main__":
-    alphabet = string.ascii_lowercase + string.digits
-    repeats = 6
-    filename = "products.txt"
-    pg = ProductGenerator(alphabet, repeats, filename)
-    asyncio.run(pg.generate_products())
+    asyncio.run(main())
